ac33ef4621fa54d410a34d044484c091e217dd63
João Bolsson <joaobolsson@animati.com.br>
2019-10-21 16:36:12 -0300

    simulacoes

--- Trab3/src/br/com/jvmarques/Client.java
+++ Trab3/src/br/com/jvmarques/Client.java
@@ -7,4 +7,29 @@ package br.com.jvmarques;
  */
 public class Client {
 
+    /**
+     * Main function.
+     *
+     * @param args Command line arguments (ignored).
+     */
+    public static void main(final String[] args) {
+        System.out.println("Simulação Soma");
+        Variable a = new Variable("5");
+        Variable b = new Variable("4");
+
+        Adder exp = new Adder(a, b);
+
+        int interpret = exp.interpret(null);
+        System.out.println("result: " + interpret);
+
+        System.out.println("-----------------------------");
+        System.out.println("Simulação Subtração");
+        Subtractor subtractor = new Subtractor(a, b);
+
+        interpret = subtractor.interpret(null);
+        System.out.println("result: " + interpret);
+
+        System.out.println("Simulação Soma de duas Expressões");
+        System.out.println("result: " + new Adder(exp, subtractor).interpret(null));
+    }
 }
